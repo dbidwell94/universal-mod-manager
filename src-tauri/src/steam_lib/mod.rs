3d13eb074c97d9311272158ec3487c9fe00b62b3
Devin Bidwell <dbidwell94@gmail.com>
2022-09-19 10:43:14 -0700

    wip

--- src-tauri/src/steam_lib/mod.rs
+++ src-tauri/src/steam_lib/mod.rs
@@ -1,9 +1,9 @@
 #[cfg(target_os = "windows")]
-const STEAM_PATH: &'static str = "";
+const STEAM_PATH: &'static str = "C:/Program Files (x86)/Steam/steamapps/common";
 #[cfg(target_os = "linux")]
 const STEAM_PATH: &'static str = "~/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common";
 #[cfg(target_os = "macos")]
-const STEAM_PATH: &'static str = "";
+const STEAM_PATH: &'static str = "~/Library/Application Support/Steam/SteamApps/common";
 
 use anyhow::Result as AnyhowResult;
 
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,29 +1,42 @@
-import styled from "@emotion/styled";
 import { Paper } from "@mui/material";
+import styled from "@emotion/styled";
 import { Box } from "@mui/system";
 import { useState } from "react";
 
-const SquareImg = styled.img<{ hovered?: boolean }>`
+const SquareImg = styled.img`
   height: 100%;
   width: auto;
+  filter: blur(0.0625);
   transition: 0.0625s ease-in-out border-color, 0.0625s ease-in-out filter;
+  margin: 0 0.25rem;
   border: thin solid transparent;
-  border-color: ${({ hovered }) => (hovered ? "white" : "transparent")};
-  filter: ${({ hovered }) => (hovered ? "blur(0)" : "blur(.0625rem)")};
+  border-color: transparent;
+  &:first-child {
+    margin-left: 0;
+  }
+  &:last-child {
+    margin-right: 0;
+  }
+  &:hover {
+    border-color: white;
+    filter: blur(0);
+  }
 `;
 
 export default function TopBar() {
-  const [imgIsHovered, setImgIsHovered] = useState(false);
+  const [installedGames, setInstalledGames] = useState(["Valheim", "Stormworks", "Skyrim", "Valheim", "Stormworks", "Skyrim", "Valheim", "Stormworks", "Skyrim"]);
 
   return (
     <Paper sx={{ py: 0, height: "8rem" }}>
-      <Box sx={{ display: "flex", height: "100%", width: "auto" }}>
-        <SquareImg
-          src='https://upload.wikimedia.org/wikipedia/en/7/77/Valheim_2021_logo.jpg'
-          onMouseEnter={() => setImgIsHovered(true)}
-          onMouseLeave={() => setImgIsHovered(false)}
-          hovered={imgIsHovered}
-        />
+      <Box sx={{ display: "flex", height: "100%", width: "auto", overflowX: "auto" }}>
+        {installedGames.map((gameName, index) => {
+          return (
+            <SquareImg
+              key={index}
+              src='https://upload.wikimedia.org/wikipedia/en/7/77/Valheim_2021_logo.jpg'
+            />
+          );
+        })}
       </Box>
     </Paper>
   );
